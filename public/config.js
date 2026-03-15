a1fd8b8b3a17064135eea8670bf4cac72eeb5449
Prabhat <prabhat@classfix.test>
2026-03-15 16:08:07 +0530

    Initial commit: College Complaint System

--- public/config.js
+++ public/config.js
@@ -2,4 +2,4 @@
 // When running locally on your computer, the app connects to http://localhost:3000
 
 // We are forcing the app to always use the local backend until Render is properly configured
-const API_BASE = 'http://localhost:3000';
+const API_BASE = window.location.hostname === 'localhost' ? 'http://localhost:3000' : '';
